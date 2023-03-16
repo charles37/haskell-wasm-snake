14737543db8247c385ba3208dc7fdaa0fb6bac58
charles37 <charles37@github.com>
2023-03-15 22:38:38 -0400

    fixed spacing

--- src/Main.hs
+++ src/Main.hs
@@ -1,118 +1,118 @@
-    {-# LANGUAGE JavaScriptFFI #-}
-    {-# LANGUAGE CApiFFI #-} 
-    {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE JavaScriptFFI #-}
+{-# LANGUAGE CApiFFI #-} 
+{-# LANGUAGE ScopedTypeVariables #-}
 
 
-    import qualified Data.ByteString.UTF8 as BSU 
-    import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
-    import Foreign (Ptr, copyBytes, mallocBytes, poke, malloc, free)
-    import Foreign.C (CChar)
-    import Data.IORef
-    import System.IO.Unsafe
-    import System.Random.Mersenne (randomIO)
-    import Prelude hiding (Left, Right)
+import qualified Data.ByteString.UTF8 as BSU 
+import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
+import Foreign (Ptr, copyBytes, mallocBytes, poke, malloc, free)
+import Foreign.C (CChar)
+import Data.IORef
+import System.IO.Unsafe
+import System.Random.Mersenne (randomIO)
+import Prelude hiding (Left, Right)
 
-    data Direction = Up | Down | Left | Right deriving (Show, Eq)
+data Direction = Up | Down | Left | Right deriving (Show, Eq)
 
-    type Position = (Int, Int)
+type Position = (Int, Int)
 
-    data GameState = GameState
-      { snake :: [Position]
-      , direction :: Direction
-      , food :: Position
-      } deriving (Show)
+data GameState = GameState
+  { snake :: [Position]
+  , direction :: Direction
+  , food :: Position
+  } deriving (Show)
 
-    width :: Int
-    width = 20
+width :: Int
+width = 20
 
-    height :: Int
-    height = 20
+height :: Int
+height = 20
 
-    initialGameStateIO :: IO GameState
-    initialGameStateIO = do 
-      let initialSnake = [(width `div` 2, height `div` 2)]
-      GameState initialSnake Right <$> randomPosition
+initialGameStateIO :: IO GameState
+initialGameStateIO = do 
+  let initialSnake = [(width `div` 2, height `div` 2)]
+  GameState initialSnake Right <$> randomPosition
 
-    randomPosition :: IO Position
-    randomPosition = do
-      x <- randomIO --(0, width - 1)
-      y <- randomIO --(0, height - 1)
-      let x' = x `mod` width
-      let y' = y `mod` height
-      pure (x', y')
-
-
-    initialGameState :: IORef GameState
-    initialGameState = unsafePerformIO $ newIORef =<< initialGameStateIO
-    {-# NOINLINE initialGameState #-}
+randomPosition :: IO Position
+randomPosition = do
+  x <- randomIO --(0, width - 1)
+  y <- randomIO --(0, height - 1)
+  let x' = x `mod` width
+  let y' = y `mod` height
+  pure (x', y')
 
 
-    main :: IO ()
-    main = mempty
-
-    foreign export capi mallocPtr :: IO (Ptr (Ptr a))
-
-    mallocPtr :: IO (Ptr (Ptr a))
-    mallocPtr = malloc
+initialGameState :: IORef GameState
+initialGameState = unsafePerformIO $ newIORef =<< initialGameStateIO
+{-# NOINLINE initialGameState #-}
 
 
-    foreign export capi freePtr :: Ptr a -> IO ()
+main :: IO ()
+main = mempty
 
-    freePtr :: Ptr a -> IO ()
-    freePtr = free
+foreign export capi mallocPtr :: IO (Ptr (Ptr a))
 
-
-    foreign export capi updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
-    updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-    updateGameState inputPtr inputLen outputPtrPtr = do
-
-      input <- unsafePackMallocCStringLen (inputPtr, inputLen)
-      gameState <- readIORef initialGameState
-      randomPosition' <- randomPosition
-
-      let direction' = case input of
-            "ArrowUp" -> Up
-            "ArrowDown" -> Down
-            "ArrowLeft" -> Left
-            "ArrowRight" -> Right
-            _ -> direction gameState
-      let newSnakeHead = case direction' of
-            Up -> (x, y - 1)
-            Down -> (x, y + 1)
-            Left -> (x - 1, y)
-            Right -> (x + 1, y)
-            where
-              (x, y) = head $ snake gameState
-
-      let (newFood, gotFood) = if newSnakeHead == food gameState
-            then (randomPosition', True)
-            else (food gameState, False)
+mallocPtr :: IO (Ptr (Ptr a))
+mallocPtr = malloc
 
 
-      let newSnake = if gotFood
-            then newSnakeHead : snake gameState
-            else take (length $ snake gameState) $ newSnakeHead : snake gameState
-      
+foreign export capi freePtr :: Ptr a -> IO ()
 
-      let newGameState = GameState newSnake direction' newFood
-
-      let output = gameStateToString newGameState
+freePtr :: Ptr a -> IO ()
+freePtr = free
 
 
-      writeIORef initialGameState newGameState
+foreign export capi updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
 
-      unsafeUseAsCStringLen (BSU.fromString output) $ \(buf, len) -> do
-        outputPtr <- mallocBytes len
-        poke outputPtrPtr outputPtr
-        copyBytes outputPtr buf len
-        pure len
+updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
+updateGameState inputPtr inputLen outputPtrPtr = do
 
-    gameStateToString :: GameState -> String
-    gameStateToString gameState = unlines rows
-      where
-        rows = [[charAt (x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
-        charAt pos
-          | pos `elem` snake gameState = '*'
-          | pos == food gameState = '@'
-          | otherwise = '.'
+  input <- unsafePackMallocCStringLen (inputPtr, inputLen)
+  gameState <- readIORef initialGameState
+  randomPosition' <- randomPosition
+
+  let direction' = case input of
+        "ArrowUp" -> Up
+        "ArrowDown" -> Down
+        "ArrowLeft" -> Left
+        "ArrowRight" -> Right
+        _ -> direction gameState
+  let newSnakeHead = case direction' of
+        Up -> (x, y - 1)
+        Down -> (x, y + 1)
+        Left -> (x - 1, y)
+        Right -> (x + 1, y)
+        where
+          (x, y) = head $ snake gameState
+
+  let (newFood, gotFood) = if newSnakeHead == food gameState
+        then (randomPosition', True)
+        else (food gameState, False)
+
+
+  let newSnake = if gotFood
+        then newSnakeHead : snake gameState
+        else take (length $ snake gameState) $ newSnakeHead : snake gameState
+  
+
+  let newGameState = GameState newSnake direction' newFood
+
+  let output = gameStateToString newGameState
+
+
+  writeIORef initialGameState newGameState
+
+  unsafeUseAsCStringLen (BSU.fromString output) $ \(buf, len) -> do
+    outputPtr <- mallocBytes len
+    poke outputPtrPtr outputPtr
+    copyBytes outputPtr buf len
+    pure len
+
+gameStateToString :: GameState -> String
+gameStateToString gameState = unlines rows
+  where
+    rows = [[charAt (x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
+    charAt pos
+      | pos `elem` snake gameState = '*'
+      | pos == food gameState = '@'
+      | otherwise = '.'
--- www/main.js
+++ www/main.js
@@ -30,84 +30,85 @@ canvas.height = height * cellSize;
 async function update() {
 
 
-  if (!window.direction) {
+if (!window.direction) {
     window.direction = 's';
-  }
+}
 
 
-  const inputLen = Buffer.byteLength(window.direction);
-  const inputPtr = exports.malloc(inputLen);
-  const inputArr = new Uint8Array(memory.buffer, inputPtr, inputLen);
-  encoder.encodeInto(window.direction, inputArr);
-  const outputPtrPtr = exports.mallocPtr();
-  const outputLen = exports.updateGameState(inputPtr, inputLen, outputPtrPtr);
-  const outputPtrArr = new Uint32Array(memory.buffer, outputPtrPtr, 1);
-  const outputPtr = outputPtrArr[0];
-  const outputArr = new Uint8Array(memory.buffer, outputPtr, outputLen);
-  const output = decoder.decode(outputArr);
-
-
-
-  exports.freePtr(inputPtr);
-  exports.freePtr(outputPtr);
-  exports.freePtr(outputPtrPtr);
-
-  console.log(exports) 
+const inputLen = Buffer.byteLength(window.direction);
+const inputPtr = exports.malloc(inputLen);
+const inputArr = new Uint8Array(memory.buffer, inputPtr, inputLen);
+encoder.encodeInto(window.direction, inputArr);
+const outputPtrPtr = exports.mallocPtr();
+const outputLen = exports.updateGameState(inputPtr, inputLen, outputPtrPtr);
+const outputPtrArr = new Uint32Array(memory.buffer, outputPtrPtr, 1);
+const outputPtr = outputPtrArr[0];
+const outputArr = new Uint8Array(memory.buffer, outputPtr, outputLen);
+const output = decoder.decode(outputArr);
 
 
 
-  draw(output);
-  // delay
-  await new Promise(resolve => setTimeout(resolve, 150));
+exports.freePtr(inputPtr);
+exports.freePtr(outputPtr);
+exports.freePtr(outputPtrPtr);
+
+console.log(exports) 
 
 
-  if (window.isRunning) {
+
+draw(output);
+// delay
+await new Promise(resolve => setTimeout(resolve, 150));
+
+
+if (window.isRunning) {
     window.requestAnimationFrame(update);
-  }
+}
 }
 
 
 
 function draw(gameStateStr) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  const rows = gameStateStr.trim().split('\n');
-  console.log(rows)
+const rows = gameStateStr.trim().split('\n');
+console.log(rows)
 
 
-  for (let y = 0; y < height; y++) {
+for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const c = rows[y][x];
-      if (c === '*') {
+    const c = rows[y][x];
+    if (c === '*') {
         ctx.fillStyle = 'black';
         ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-      } else if (c === '@') {
+    } else if (c === '@') {
         ctx.fillStyle = 'red';
         ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
-      }
+    }
 
     }
-  }
+}
 }
 
 document.addEventListener("keydown", event => {
-  window.direction = event.key;
-  console.log(event.key);
+window.direction = event.key;
+console.log(event.key);
 });
 
 window.start = () => {
-  window.isRunning = true;
-  console.log('start');
-  console.log(window);
-  update();
+window.isRunning = true;
+console.log('start');
+console.log(window);
+update();
 };
 
 window.stop = () => {
-  window.isRunning = false;
+window.isRunning = false;
 };
 
 function init() {
-  window.start();
+window.start();
 }
 
 init();
+
