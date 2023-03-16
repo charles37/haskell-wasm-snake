a847cbe7aecd896b9dbc7260b292ab8ef09ef68e
charles37 <charles37@github.com>
2023-03-15 22:31:41 -0400

    still has the memory leak

--- src/Main.hs
+++ src/Main.hs
@@ -1,269 +1,118 @@
--- import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
--- import Foreign (Ptr, copyBytes, malloc, mallocBytes, poke)
--- import Foreign.C (CChar)
-
--- main :: IO ()
--- main = mempty
-
--- foreign export capi mallocPtr :: IO (Ptr (Ptr a))
-
--- mallocPtr :: IO (Ptr (Ptr a))
--- mallocPtr = malloc
-
--- foreign export capi addExclamationToInput :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
--- addExclamationToInput :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
--- addExclamationToInput inputPtr inputLen outputPtrPtr = do
---   input <- unsafePackMallocCStringLen (inputPtr, inputLen)
---   unsafeUseAsCStringLen (input <> "!") $ \(buf, len) -> do
---     outputPtr <- mallocBytes len
---     poke outputPtrPtr outputPtr
---     copyBytes outputPtr buf len
---     pure len
-
--- {-# LANGUAGE JavaScriptFFI #-}
--- {-# LANGUAGE CApiFFI #-} 
--- {-# LANGUAGE ScopedTypeVariables #-}
-
--- {-# LANGUAGE MultiParamTypeClasses #-}
--- {-# LANGUAGE UndecidableInstances #-}
--- {-# LANGUAGE OverloadedStrings #-}
--- {-# LANGUAGE TypeFamilies #-}
--- {-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE FlexibleContexts #-}
--- {-# LANGUAGE FlexibleInstances #-}
-
--- import qualified Data.ByteString.UTF8 as BSU 
--- import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
--- import Foreign (Ptr, copyBytes, mallocBytes, poke, malloc)
--- import Foreign.C (CChar)
--- -- import System.Random (randomRIO)
--- import System.Random.Mersenne (randomIO)
--- import Prelude hiding (Left, Right)
-
--- data Direction = Up | Down | Left | Right
-
--- type Position = (Int, Int)
-
--- data GameState = GameState
---   { snake :: [Position]
---   , direction :: Direction
---   , food :: Position
---   }
-
--- width :: Int
--- width = 20
-
--- height :: Int
--- height = 20
-
--- snakeLength :: Int
--- snakeLength = 3
-
--- initialGameStateIO :: IO GameState
--- initialGameStateIO = do
---   let initialSnake = [(width `div` 2, height `div` 2)]
---   initialFood <- randomPosition
---   pure $ GameState initialSnake Right initialFood
-
--- randomPosition :: IO Position
--- randomPosition = do
---   x <- randomRIO (0, width - 1)
---   y <- randomRIO (0, height - 1)
---   pure (x, y)
-
--- main :: IO ()
--- main = mempty
-
--- foreign export capi mallocPtr :: IO (Ptr (Ptr a))
-
--- mallocPtr :: IO (Ptr (Ptr a))
--- mallocPtr = malloc
-
--- foreign export capi updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
--- updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
--- updateGameState inputPtr inputLen outputPtrPtr = do
---   input <- unsafePackMallocCStringLen (inputPtr, inputLen)
---   initialGameState <- initialGameStateIO
---   randomPosition' <- randomPosition
---   print input
---   let direction' = case input of
---         "ArrowUp" -> Up
---         "ArrowDown" -> Down
---         "ArrowLeft" -> Left
---         "ArrowRight" -> Right
---         _ -> direction initialGameState
---   let newSnakeHead = case direction' of
---         Up -> (x, y - 1)
---         Down -> (x, y + 1)
---         Left -> (x - 1, y)
---         Right -> (x + 1, y)
---         where
---           (x, y) = head $ snake initialGameState
---   let newSnake = if length newSnakeHead > snakeLength
---         then take snakeLength $ newSnakeHead : snake initialGameState
---         else newSnakeHead : snake initialGameState
---   let newFood = if newSnakeHead == food initialGameState
---         then randomPosition'
---         else food initialGameState
---   let newGameState = GameState newSnake direction' newFood
---   let output = gameStateToString newGameState
---   unsafeUseAsCStringLen (BSU.fromString output) $ \(buf, len) -> do
---     outputPtr <- mallocBytes len
---     poke outputPtrPtr outputPtr
---     copyBytes outputPtr buf len
---     pure len
-
--- gameStateToString :: GameState -> String
--- gameStateToString gameState = unlines rows
---   where
---     rows = [[charAt (x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
---     charAt pos
---       | pos `elem` snake gameState = '*'
---       | pos == food gameState = '@'
---       | otherwise = ' '
+    {-# LANGUAGE JavaScriptFFI #-}
+    {-# LANGUAGE CApiFFI #-} 
+    {-# LANGUAGE ScopedTypeVariables #-}
 
 
+    import qualified Data.ByteString.UTF8 as BSU 
+    import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
+    import Foreign (Ptr, copyBytes, mallocBytes, poke, malloc, free)
+    import Foreign.C (CChar)
+    import Data.IORef
+    import System.IO.Unsafe
+    import System.Random.Mersenne (randomIO)
+    import Prelude hiding (Left, Right)
+
+    data Direction = Up | Down | Left | Right deriving (Show, Eq)
+
+    type Position = (Int, Int)
+
+    data GameState = GameState
+      { snake :: [Position]
+      , direction :: Direction
+      , food :: Position
+      } deriving (Show)
+
+    width :: Int
+    width = 20
+
+    height :: Int
+    height = 20
+
+    initialGameStateIO :: IO GameState
+    initialGameStateIO = do 
+      let initialSnake = [(width `div` 2, height `div` 2)]
+      GameState initialSnake Right <$> randomPosition
+
+    randomPosition :: IO Position
+    randomPosition = do
+      x <- randomIO --(0, width - 1)
+      y <- randomIO --(0, height - 1)
+      let x' = x `mod` width
+      let y' = y `mod` height
+      pure (x', y')
 
 
+    initialGameState :: IORef GameState
+    initialGameState = unsafePerformIO $ newIORef =<< initialGameStateIO
+    {-# NOINLINE initialGameState #-}
 
 
+    main :: IO ()
+    main = mempty
+
+    foreign export capi mallocPtr :: IO (Ptr (Ptr a))
+
+    mallocPtr :: IO (Ptr (Ptr a))
+    mallocPtr = malloc
 
 
-{-# LANGUAGE JavaScriptFFI #-}
-{-# LANGUAGE CApiFFI #-} 
-{-# LANGUAGE ScopedTypeVariables #-}
+    foreign export capi freePtr :: Ptr a -> IO ()
 
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-
-import qualified Data.ByteString.UTF8 as BSU 
-import Data.ByteString.Unsafe (unsafePackMallocCStringLen, unsafeUseAsCStringLen)
-import Foreign (Ptr, copyBytes, mallocBytes, poke, malloc)
-import Foreign.C (CChar)
--- import System.Random (randomRIO)
-import Data.IORef
-import System.IO.Unsafe
-import System.Random.Mersenne (randomIO)
-import Prelude hiding (Left, Right)
-import System.Mem
-
-data Direction = Up | Down | Left | Right deriving (Show, Eq)
-
-type Position = (Int, Int)
-
-data GameState = GameState
-  { snake :: [Position]
-  , direction :: Direction
-  , food :: Position
-  } deriving (Show)
-
-width :: Int
-width = 20
-
-height :: Int
-height = 20
-
-snakeLength :: Int
-snakeLength = 3
-
-initialGameStateIO :: IO GameState
-initialGameStateIO = do
-  let initialSnake = [(width `div` 2, height `div` 2)]
-  initialFood <- randomPosition
-  pure $ GameState initialSnake Right initialFood
-
-randomPosition :: IO Position
-randomPosition = do
-  x <- randomIO --(0, width - 1)
-  y <- randomIO --(0, height - 1)
-  let x' = x `mod` width
-  let y' = y `mod` height
-  pure (x', y')
+    freePtr :: Ptr a -> IO ()
+    freePtr = free
 
 
-initialGameState :: IORef GameState
-initialGameState = unsafePerformIO $ newIORef =<< initialGameStateIO
-{-# NOINLINE initialGameState #-}
+    foreign export capi updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
+
+    updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
+    updateGameState inputPtr inputLen outputPtrPtr = do
+
+      input <- unsafePackMallocCStringLen (inputPtr, inputLen)
+      gameState <- readIORef initialGameState
+      randomPosition' <- randomPosition
+
+      let direction' = case input of
+            "ArrowUp" -> Up
+            "ArrowDown" -> Down
+            "ArrowLeft" -> Left
+            "ArrowRight" -> Right
+            _ -> direction gameState
+      let newSnakeHead = case direction' of
+            Up -> (x, y - 1)
+            Down -> (x, y + 1)
+            Left -> (x - 1, y)
+            Right -> (x + 1, y)
+            where
+              (x, y) = head $ snake gameState
+
+      let (newFood, gotFood) = if newSnakeHead == food gameState
+            then (randomPosition', True)
+            else (food gameState, False)
 
 
-main :: IO ()
-main = mempty
+      let newSnake = if gotFood
+            then newSnakeHead : snake gameState
+            else take (length $ snake gameState) $ newSnakeHead : snake gameState
+      
 
-foreign export capi mallocPtr :: IO (Ptr (Ptr a))
+      let newGameState = GameState newSnake direction' newFood
 
-mallocPtr :: IO (Ptr (Ptr a))
-mallocPtr = malloc
-
-foreign export capi updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-
-
-updateGameState :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
-updateGameState inputPtr inputLen outputPtrPtr = do
-
-  input <- unsafePackMallocCStringLen (inputPtr, inputLen)
-  gameState <- readIORef initialGameState
-  randomPosition' <- randomPosition
-
-  let direction' = case input of
-        "ArrowUp" -> Up
-        "ArrowDown" -> Down
-        "ArrowLeft" -> Left
-        "ArrowRight" -> Right
-        _ -> direction gameState
-  let newSnakeHead = case direction' of
-        Up -> (x, y - 1)
-        Down -> (x, y + 1)
-        Left -> (x - 1, y)
-        Right -> (x + 1, y)
-        where
-          (x, y) = head $ snake gameState
-
-  let (newFood, gotFood) = if newSnakeHead == food gameState
-        then (randomPosition', True)
-        else (food gameState, False)
+      let output = gameStateToString newGameState
 
 
-  let newSnake = if gotFood
-        then newSnakeHead : snake gameState
-        else take (length $ snake gameState) $ newSnakeHead : snake gameState
-  
+      writeIORef initialGameState newGameState
 
-  let newGameState = GameState newSnake direction' newFood
+      unsafeUseAsCStringLen (BSU.fromString output) $ \(buf, len) -> do
+        outputPtr <- mallocBytes len
+        poke outputPtrPtr outputPtr
+        copyBytes outputPtr buf len
+        pure len
 
-  let output = gameStateToString newGameState
-
-
-  writeIORef initialGameState newGameState
-
-  unsafeUseAsCStringLen (BSU.fromString output) $ \(buf, len) -> do
-    outputPtr <- mallocBytes len
-    poke outputPtrPtr outputPtr
-    copyBytes outputPtr buf len
-    pure len
-  
-
--- addExclamationToInput :: Ptr CChar -> Int -> Ptr (Ptr CChar) -> IO Int
--- addExclamationToInput inputPtr inputLen outputPtrPtr = do
---   input <- unsafePackMallocCStringLen (inputPtr, inputLen)
---   unsafeUseAsCStringLen (input <> "!") $ \(buf, len) -> do
---     outputPtr <- mallocBytes len
---     poke outputPtrPtr outputPtr
---     copyBytes outputPtr buf len
---     pure len
-
-
-gameStateToString :: GameState -> String
-gameStateToString gameState = unlines rows
-  where
-    rows = [[charAt (x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
-    charAt pos
-      | pos `elem` snake gameState = '*'
-      | pos == food gameState = '@'
-      | otherwise = '.'
+    gameStateToString :: GameState -> String
+    gameStateToString gameState = unlines rows
+      where
+        rows = [[charAt (x, y) | x <- [0..width - 1]] | y <- [0..height - 1]]
+        charAt pos
+          | pos `elem` snake gameState = '*'
+          | pos == food gameState = '@'
+          | otherwise = '.'
--- www/main.js
+++ www/main.js
@@ -1,48 +1,3 @@
-// import { WASI } from "@bjorn3/browser_wasi_shim/src";
-
-// const wasi = new WASI([], [], []);
-// const wasiImportObj = { wasi_snapshot_preview1: wasi.wasiImport };
-// const wasm = await WebAssembly.instantiateStreaming(fetch("experiment.wasm"), wasiImportObj);
-// wasi.inst = wasm.instance;
-// const exports = wasm.instance.exports;
-// const memory = exports.memory;
-// const encoder = new TextEncoder();
-// const decoder = new TextDecoder();
-
-// exports._initialize();
-// exports.hs_init(0, 0);
-
-// async function handleKey(key) {
-//     const inputLen = Buffer.byteLength(key);
-//     const inputPtr = exports.malloc(inputLen);
-//     const inputArr = new Uint8Array(memory.buffer, inputPtr, inputLen);
-//     encoder.encodeInto(key, inputArr);
-
-//     const outputPtrPtr = exports.mallocPtr();
-//     const outputLen = exports.addExclamationToInput(inputPtr, inputLen, outputPtrPtr);
-//     const outputPtrArr = new Uint32Array(memory.buffer, outputPtrPtr, 1);
-//     const outputPtr = outputPtrArr[0];
-//     const outputArr = new Uint8Array(memory.buffer, outputPtr, outputLen);
-//     const output = decoder.decode(outputArr);
-//     console.log(output);
-//     exports.free(outputPtr);
-// }
-
-// document.addEventListener("keydown", event => handleKey(event.key), false);
-
-
-// import { WASI } from "@bjorn3/browser_wasi_shim/src";
-
-// const wasi = new WASI([], [], []);
-
-// const wasiImportObj = { wasi_snapshot_preview1: wasi.wasiImport };
-// const wasm = await WebAssembly.instantiateStreaming(fetch("experiment.wasm"), wasiImportObj);
-// wasi.inst = wasm.instance;
-// const exports = wasm.instance.exports;
-// const memory = exports.memory;
-// const encoder = new TextEncoder();
-// const decoder = new TextDecoder();
-
 import { WASI } from "@bjorn3/browser_wasi_shim/src";
 
 const wasi = new WASI([], [], []);
@@ -74,7 +29,13 @@ canvas.height = height * cellSize;
 
 async function update() {
 
-  const inputLen = Buffer.byteLength(window.direction || ' ');
+
+  if (!window.direction) {
+    window.direction = 's';
+  }
+
+
+  const inputLen = Buffer.byteLength(window.direction);
   const inputPtr = exports.malloc(inputLen);
   const inputArr = new Uint8Array(memory.buffer, inputPtr, inputLen);
   encoder.encodeInto(window.direction, inputArr);
@@ -87,13 +48,11 @@ async function update() {
 
 
 
+  exports.freePtr(inputPtr);
+  exports.freePtr(outputPtr);
+  exports.freePtr(outputPtrPtr);
 
-
-  exports.free(inputPtr);
-  exports.free(outputPtr);
-  exports.free(outputPtrPtr);
-
-  console.log(exports)
+  console.log(exports) 
 
 
 
